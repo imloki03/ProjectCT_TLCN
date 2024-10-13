900e6ca1a8b1648319034824a9ec0860c20b01d9
Nguyen Van Thi <126433649+imloki03@users.noreply.github.com>
2024-10-13 21:42:46 +0700

    Initial commit

---
+++ src/main/java/com/hcmute/projectCT/ProjectCtTlcnApplication.java
@@ -0,0 +1,13 @@
+package com.hcmute.projectCT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCtTlcnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCtTlcnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hcmute/projectCT/ProjectCtTlcnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcmute.projectCT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCtTlcnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
