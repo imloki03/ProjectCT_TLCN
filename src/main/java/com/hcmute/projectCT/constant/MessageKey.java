51d4a55483e6f1066f6d68ee6eb6b5eebfaed373
phancong1609 <126433670+phancong1609@users.noreply.github.com>
2024-10-15 20:21:35 +0700

    Implement register usecase

--- src/main/java/com/hcmute/projectCT/constant/MessageKey.java
+++ src/main/java/com/hcmute/projectCT/constant/MessageKey.java
@@ -3,4 +3,7 @@ package com.hcmute.projectCT.constant;
 public class MessageKey {
     public static final String LOGIN_SUCCESS = "auth.login.success";
     public static final String LOGIN_FAILED = "auth.login.failed";
+    public static final String REGISTER_SUCCESS = "user.reg.success";
+    public static final String EMAIL_ALREADY_EXISTS = "user.reg.email";
+    public static final String SERVER_ERROR = "server.error";
 }
--- src/main/java/com/hcmute/projectCT/repository/UserRepository.java
+++ src/main/java/com/hcmute/projectCT/repository/UserRepository.java
@@ -3,8 +3,11 @@ package com.hcmute.projectCT.repository;
 import com.hcmute.projectCT.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
     User findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
 }
