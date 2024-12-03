8ee501aa7a9d7bbc16160a790754abdcc8b71858
Nguyen Van Thi <xc.lokigod@gmail.com>
2024-12-03 21:26:21 +0700

    Fix cors for websocket

--- src/main/java/com/hcmute/projectCT/configuration/WebsocketConfig.java
+++ src/main/java/com/hcmute/projectCT/configuration/WebsocketConfig.java
@@ -19,7 +19,7 @@ public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:3000", "http://localhost:8081")
+        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:3000", "http://localhost:8081", "https://projectct.vercel.app")
                 .withSockJS();
     }
 }
