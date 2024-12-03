28658215db53b468ba4190dee636be252ac8b71c
Nguyen Van Thi <xc.lokigod@gmail.com>
2024-12-03 16:59:53 +0700

    Update cors

--- src/main/java/com/hcmute/projectCT/configuration/WebSecurityConfig.java
+++ src/main/java/com/hcmute/projectCT/configuration/WebSecurityConfig.java
@@ -61,7 +61,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
