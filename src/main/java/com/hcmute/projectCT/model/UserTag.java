b464c229c11102310d1f1d4457723b5057b863e1
phancong1609 <126433670+phancong1609@users.noreply.github.com>
2024-12-03 14:46:05 +0700

    Fix non-primary key in mysql

--- src/main/java/com/hcmute/projectCT/model/UserTag.java
+++ src/main/java/com/hcmute/projectCT/model/UserTag.java
@@ -1,7 +1,11 @@
 package com.hcmute.projectCT.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "user_tags")
 public class UserTag {
