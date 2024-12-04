e1e4cafbc771d9ae54e502345ac3143fd806beac
Nguyen Van Thi <xc.lokigod@gmail.com>
2024-12-04 18:34:41 +0700

    Fix gemini

--- src/main/java/com/hcmute/projectCT/configuration/GeminiConfig.java
+++ src/main/java/com/hcmute/projectCT/configuration/GeminiConfig.java
@@ -4,9 +4,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
-@Configuration
+@Component
 @Slf4j
 @RequiredArgsConstructor
 public class GeminiConfig {
